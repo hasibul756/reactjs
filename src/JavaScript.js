467f010528ebc7aabe34ffb1b6dca5ca72f6682b
hasibul756 <109759157+hasibul756@users.noreply.github.com>
2024-04-04 10:33:52 +0530

    Update JavaScript.js

---
+++ src/JavaScript.js
@@ -0,0 +1,9 @@
+// String Indices
+
+// let name = 'Hasibul Alam';
+// for(let i =0;i<name.length;i++) {
+//     console.log(name[i]);
+// }
+
+// let name = "Hasibul Alam";
+// console.log(name[0]);
