2616ef9a7c7b84ae2ff6fc4f43b0ffe85876731a
hasibul756 <hasibul756@gmail.com>
2024-04-29 00:58:08 +0530

    Create FetchAPI.js

---
+++ src/javascript/FetchAPI.js
@@ -0,0 +1,13 @@
+// fetch API
+
+// The Fetch API provides an interface for fetching (sending/receiving) resources.
+
+// It uses Request and Response objects.
+// The fetch() method id used to fetch recourses (data).API
+
+// let promise = fetch(url, [options])
+
+// API = Application Programming Interface
+
+
+
