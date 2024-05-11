07a00b2506abe752e4e9173c9c14888ddb581392
hasibul756 <109759157+hasibul756@users.noreply.github.com>
2024-05-11 17:54:06 +0530

    Update Closures.js

--- src/javascript/Closures.js
+++ src/javascript/Closures.js
@@ -2,16 +2,29 @@
 
 // Lexical Scoping:
 
-// function init() {
-//     let name = "Sahil";
-//     function displayName() {
-//       console.log(name);
-//     }
-//     displayName();
-//   }
+// when a function is defined within another function, the inner function has access to variables declared in the outer function.
+// This is because JavaScript uses lexical scoping.
+
+// Note:--
+// Variables created without a declaration keyword (var, let, or const) are always global, even if they are created inside a function.
+
+function init() {
+    let name = "Sahil";
+    function displayName() {
+      console.log(name);
+    }
+    displayName();
+  }
 // init();
 
 function outer() {
     let user = "Sahil";
+    function inner() {
+        // can access variable of outer function.
+        console.log(user)
+    }
+    inner();
 }
-console.log(user);
+outer();
+console.log("outer", user);
+
