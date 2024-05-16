e4fae87ae6219d10af0137dcc6f25fb25485abef
hasibul756 <109759157+hasibul756@users.noreply.github.com>
2024-05-16 18:09:56 +0530

    Update JavaScript.js

--- src/JavaScript.js
+++ src/JavaScript.js
@@ -30,8 +30,8 @@
 
 // 
 
-for(let i =0;i<=100;i++) {
-    if(i%5 == 0) {
-        console.log(i);
-    }
-}
+// for(let i =0;i<=100;i++) {
+//     if(i%5 == 0) {
+//         console.log(i);
+//     }
+// }
